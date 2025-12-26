1a4f6baebea5f0e635d97e351a539cf0ea1c0554
Prayash Bhattarai <prayashbhattarai64@gmail.com>
2025-12-26 17:13:51 +0545

    Refactor traffic light initialization and queue methods

--- src/traffic_simulation.cpp
+++ src/traffic_simulation.cpp
@@ -1,66 +1,80 @@
-#include <iostream>
-#include <cmath>
-#include <SDL.h>
-
 #include "traffic_simulation.hpp"
+#include <iostream>
+#include <cstdlib>
 
-Queue laneQueues[4];
 
-
-int lanePriorities[4] = { 0, 0, 0, 0 };
+Queue laneQueues[4]; 
 
 
 const SDL_Color VEHICLE_COLORS[] = {
-    { 0,   0, 255, 255 },   
-    { 255, 0,   0, 255 },   
-    { 0,   0, 128, 255 },   
-    { 255, 69,  0, 255 }    
+    {0, 0, 255, 255},    // REGULAR_CAR: Blue
+    {255, 0, 0, 255},    // AMBULANCE: Red
+    {0, 0, 128, 255},    // POLICE_CAR: Dark Blue
+    {255, 69, 0, 255}    // FIRE_TRUCK: Orange-Red
 };
 
 
-void initializeTrafficLights(TrafficLight* lights)
-{
-    
+Queue::~Queue() {
+    while (!isEmpty()) {
+        dequeue();
+    }
+}
+
+void Queue::init() {
+    front = rear = nullptr;
+    size = 0;
+}
+
+void Queue::enqueue(const Vehicle& vehicle) {
+    Node* node = new Node;
+    node->vehicle = vehicle;
+    node->next = nullptr;
+
+    if (rear == nullptr) {
+        front = rear = node;
+    } else {
+        rear->next = node;
+        rear = node;
+    }
+    size++;
+}
+
+Vehicle Queue::dequeue() {
+    if (front == nullptr) {
+        return Vehicle(); 
+    }
+    Node* temp = front;
+    Vehicle v = temp->vehicle;
+    front = front->next;
+    if (front == nullptr) rear = nullptr;
+    delete temp;
+    size--;
+    return v;
+}
+
+bool Queue::isEmpty() const {
+    return front == nullptr;
+}
+
+
+void initializeTrafficLights(TrafficLight* lights) {
     lights[0].state = TrafficLightState::RED;
     lights[0].timer = 0;
+    lights[0].position = { INTERSECTION_X - LANE_WIDTH, INTERSECTION_Y - LANE_WIDTH - TRAFFIC_LIGHT_HEIGHT, TRAFFIC_LIGHT_WIDTH, TRAFFIC_LIGHT_HEIGHT };
     lights[0].direction = Direction::NORTH;
-    lights[0].position = {
-        INTERSECTION_X - TRAFFIC_LIGHT_WIDTH / 2,
-        INTERSECTION_Y - LANE_WIDTH - 20,
-        TRAFFIC_LIGHT_WIDTH,
-        TRAFFIC_LIGHT_HEIGHT
-    };
 
-   
     lights[1].state = TrafficLightState::RED;
     lights[1].timer = 0;
+    lights[1].position = { INTERSECTION_X - LANE_WIDTH, INTERSECTION_Y + LANE_WIDTH, TRAFFIC_LIGHT_WIDTH, TRAFFIC_LIGHT_HEIGHT };
     lights[1].direction = Direction::SOUTH;
-    lights[1].position = {
-        INTERSECTION_X - TRAFFIC_LIGHT_WIDTH / 2,
-        INTERSECTION_Y + LANE_WIDTH,
-        TRAFFIC_LIGHT_WIDTH,
-        TRAFFIC_LIGHT_HEIGHT
-    };
 
-   
     lights[2].state = TrafficLightState::GREEN;
     lights[2].timer = 0;
+    lights[2].position = { INTERSECTION_X + LANE_WIDTH, INTERSECTION_Y - LANE_WIDTH, TRAFFIC_LIGHT_HEIGHT, TRAFFIC_LIGHT_WIDTH };
     lights[2].direction = Direction::EAST;
-    lights[2].position = {
-        INTERSECTION_X + LANE_WIDTH,
-        INTERSECTION_Y - TRAFFIC_LIGHT_HEIGHT / 2,
-        TRAFFIC_LIGHT_HEIGHT,
-        TRAFFIC_LIGHT_WIDTH
-    };
 
-   
     lights[3].state = TrafficLightState::GREEN;
     lights[3].timer = 0;
+    lights[3].position = { INTERSECTION_X - LANE_WIDTH - TRAFFIC_LIGHT_HEIGHT, INTERSECTION_Y - LANE_WIDTH, TRAFFIC_LIGHT_HEIGHT, TRAFFIC_LIGHT_WIDTH };
     lights[3].direction = Direction::WEST;
-    lights[3].position = {
-        INTERSECTION_X - LANE_WIDTH - TRAFFIC_LIGHT_HEIGHT,
-        INTERSECTION_Y - TRAFFIC_LIGHT_HEIGHT / 2,
-        TRAFFIC_LIGHT_HEIGHT,
-        TRAFFIC_LIGHT_WIDTH
-    };
 }
